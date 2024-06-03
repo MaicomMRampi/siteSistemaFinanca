9be649cfa48625ae9a2443fdb45272240578b668
Maicom <maicom.azus@gmail.com>
2024-06-03 17:14:30 -0300

    Adicionando mais interações na tela

--- src/components/Solucoes.tsx
+++ src/components/Solucoes.tsx
@@ -10,9 +10,11 @@ export default function TabsDemo() {
             value: "Despesas",
             content: (
                 <div className="w-full overflow-hidden relative h-full rounded-2xl p-2 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 grid grid-cols-1 md:grid-cols-2">
-                    <p>Controle de Despesas
-                        <p className="text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
-                    </p>
+                    <div className="hidden md:block">
+                        <p>Controle de Despesas
+                            <p className="text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
+                        </p>
+                    </div>
                     <div className="bg-cover w-full bg-no-repeat bg-[url('/imagesHome/despesas.png')] rounded-2xl"></div>
                 </div>
             ),
@@ -21,8 +23,13 @@ export default function TabsDemo() {
             title: "Dashboard",
             value: "Dashboard",
             content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 ">
-                    <p>Dashboard tab</p>
+                <div className="w-full overflow-hidden relative h-full rounded-2xl p-2 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 grid grid-cols-1 md:grid-cols-2">
+                    <div className="hidden md:block">
+                        <p>Dashboard
+                            <p className="text-lg py-6">Nossa dashboard é muito mais do que uma simples tela: é o seu centro de comando financeiro, oferecendo uma visão geral clara, intuitiva de como seu dinheiro está sendo usado, categorizado e organizado.</p>
+                        </p>
+                    </div>
+                    <div className="bg-cover w-full bg-no-repeat bg-[url('/imagesHome/DASHBOARD.png')] rounded-2xl"></div>
                 </div>
             ),
         },
@@ -31,9 +38,11 @@ export default function TabsDemo() {
             value: "Investimentos",
             content: (
                 <div className="w-full overflow-hidden relative h-full rounded-2xl p-2 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 grid grid-cols-1 md:grid-cols-2">
-                    <p className=" xs:block ">Controle de Investimento
-                        <p className=" text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
-                    </p>
+                    <div className="hidden md:block">
+                        <p>Controle de Investimento
+                            <p className="text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
+                        </p>
+                    </div>
                     <div className="bg-cover w-full bg-no-repeat bg-[url('/imagesHome/investimento.png')] rounded-2xl xs:grid xs:grid-cols-1"> </div>
                 </div>
             ),

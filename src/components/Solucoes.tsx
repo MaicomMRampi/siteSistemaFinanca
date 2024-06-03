7628406071386e3c68fa7fa049d3b19d1543c604
Maicom <maicom.azus@gmail.com>
2024-06-03 16:27:04 -0300

    Ajustanao

--- src/components/Solucoes.tsx
+++ src/components/Solucoes.tsx
@@ -9,8 +9,11 @@ export default function TabsDemo() {
             title: "Despesas",
             value: "Despesas",
             content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-                    <p>Controle de Despesas</p>
+                <div className="w-full overflow-hidden relative h-full rounded-2xl p-2 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 grid grid-cols-1 md:grid-cols-2">
+                    <p>Controle de Despesas
+                        <p className="text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
+                    </p>
+                    <div className="bg-cover w-full bg-no-repeat bg-[url('/imagesHome/despesas.png')] rounded-2xl"></div>
                 </div>
             ),
         },
@@ -18,38 +21,24 @@ export default function TabsDemo() {
             title: "Dashboard",
             value: "Dashboard",
             content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 ">
                     <p>Dashboard tab</p>
                 </div>
             ),
         },
         {
-            title: "Playground",
-            value: "playground",
+            title: "Investimentos",
+            value: "Investimentos",
             content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-                    <p>Playground tab</p>
+                <div className="w-full overflow-hidden relative h-full rounded-2xl p-2 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-500 to-blue-500 grid grid-cols-1 md:grid-cols-2">
+                    <p className=" xs:block ">Controle de Investimento
+                        <p className=" text-lg py-6">Visualize seus gastos de forma organizada, classificados por categorias como alimentação, transporte, lazer, moradia, etc.</p>
+                    </p>
+                    <div className="bg-cover w-full bg-no-repeat bg-[url('/imagesHome/investimento.png')] rounded-2xl xs:grid xs:grid-cols-1"> </div>
                 </div>
             ),
-        },
-        {
-            title: "Content",
-            value: "content",
-            content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-                    <p>Content tab</p>
-                </div>
-            ),
-        },
-        {
-            title: "Random",
-            value: "random",
-            content: (
-                <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-                    <p>Random tab</p>
-                </div>
-            ),
-        },
+        }
+
     ];
 
     return (

9b8f6b6ce7584b6b916bee32492f073982121f2f
Maicom <maicom.azus@gmail.com>
2024-06-07 10:19:29 -0300

    Ajustando o layoult

--- src/components/Sistema.tsx
+++ src/components/Sistema.tsx
@@ -6,21 +6,26 @@ const words = ["suas finanças", "seus investimentos", " suas despesas"];
 export default function Home() {
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:p-10 h-[500px]">
-                <div className="md:py-14">
-                    <p className="font-extrabold text-6xl">Organize
+            <div className="min-h-[500px] grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-8 p-4">
+                <div className="flex flex-col justify-center">
+                    <p className="font-extrabold text-4xl sm:text-5xl md:text-6xl">
+                        Organize
                         <span className="font-bold">
-                            <FlipWords
-                                words={words}
-                            />
+                            <FlipWords words={words} />
                         </span>
                     </p>
-                    <p className="text-2xl font-extralight"> online, completo e gratuito.</p>
+                    <p className="text-lg sm:text-xl md:text-2xl font-extralight mt-4">
+                        online, completo e gratuito.
+                    </p>
                 </div>
-                <div className="bg-cover bg-center bg-no-repeat bg-[url('/imagesHome/home.jpg')]">
+                <div className="w-full relative flex justify-center items-center">
+                    <img className="h-[400px]" src="/imagesHome/currency-animate.svg" alt="Svg" />
+                </div>
+                <div className="col-span-1 sm:col-span-2">
+                    <h1 className="text-center text-xl sm:text-2xl ">Sistema com controle de investimento integrado</h1>
                 </div>
             </div>
-            <h1 className="text-center text-2xl">Sistema com controle de investimento integrado</h1>
+
         </>
     )
 }
